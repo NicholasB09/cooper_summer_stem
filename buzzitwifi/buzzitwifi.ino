3ba0836dda7ee1fd2e4e851895cd38ff0f3010c4
jacobevfe-prog <jacobevfe@gmail.com>
2026-08-12 10:06:30 -0400

    added dog animation

--- buzzitwifi/buzzitwifi.ino
+++ buzzitwifi/buzzitwifi.ino
@@ -150,8 +150,10 @@ void loop() {
 
       if (client.available()) {
         String incomingMsg = client.readStringUntil('\n');
-        incomingMsg.trim(); // Removes '\r' and whitespace
-
+        incomingMsg.trim();
+         // Removes '\r' and whitespace
+         u8g2.drawXBMP(0, 0, 128, 50, (const unsigned char*)pgm_read_ptr(&(epd_bitmap_Dog[counter])))
+          counter = (counter + 1) % 22; 
         Serial.print("Received line: ");
         Serial.println(incomingMsg);
       }
